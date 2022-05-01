a3da348da25e85554b045d564d768f27762d1b06
DzG <100204919+Dzgaza@users.noreply.github.com>
2022-05-01 14:53:38 +0200

    end of navbar

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const langue = document.getElementById("menu2");
+const langue = document.getElementById("menu");
+const langues = document.getElementById("main");
 
-console.log(langue);
+langue.addEventListener("mouseover", () => {
+  langues.style.display = "grid";
+});
+langues.addEventListener("mouseover", () => {
+  langues.style.display = "grid";
+});
+langue.addEventListener("mouseout", () => {
+  langues.style.display = "none";
+});
+langues.addEventListener("mouseout", () => {
+  langues.style.display = "none";
+});
+
+console.log(langues);
