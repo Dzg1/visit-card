476f10bb6a09871fe65509edcf5e1b9cda79ec2d
DzG <100204919+Dzgaza@users.noreply.github.com>
2022-05-04 17:49:46 +0200

    04/05 1749 rso

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const langue = document.getElementById("menu");
 const langues = document.getElementById("main");
-const logorso = document.getElementsByClassName("iconrso");
-const spanrso = document.getElementsByClassName("spanrso");
+const logorso1 = document.getElementById("iconrso1");
+const spanrso1 = document.getElementById("spanrso1");
+const logorso2 = document.getElementById("iconrso2");
+const spanrso2 = document.getElementById("spanrso2");
+const logorso3 = document.getElementById("iconrso3");
+const spanrso3 = document.getElementById("spanrso3");
+const logorso4 = document.getElementById("iconrso4");
+const spanrso4 = document.getElementById("spanrso4");
+const logorso5 = document.getElementById("iconrso5");
+const spanrso5 = document.getElementById("spanrso5");
 
 langue.addEventListener("mouseover", () => {
   langues.style.display = "grid";
@@ -16,9 +24,38 @@ langues.addEventListener("mouseout", () => {
   langues.style.display = "none";
 });
 
-logorso.addEventListener("mouseover", () => {
-  spanrso.style.opacity = "1";
+/*****RESEAUX******/
+
+logorso1.addEventListener("mouseover", () => {
+  spanrso1.style.opacity = "1";
 });
-logorso.addEventListener("mouseout", () => {
-  spanrso.style.opacity = "0";
+logorso1.addEventListener("mouseout", () => {
+  spanrso1.style.opacity = "0";
+});
+
+logorso2.addEventListener("mouseover", () => {
+  spanrso2.style.opacity = "1";
+});
+logorso2.addEventListener("mouseout", () => {
+  spanrso2.style.opacity = "0";
+});
+
+logorso3.addEventListener("mouseover", () => {
+  spanrso3.style.opacity = "1";
+});
+logorso3.addEventListener("mouseout", () => {
+  spanrso3.style.opacity = "0";
+});
+
+logorso4.addEventListener("mouseover", () => {
+  spanrso4.style.opacity = "1";
+});
+logorso4.addEventListener("mouseout", () => {
+  spanrso4.style.opacity = "0";
+});
+logorso5.addEventListener("mouseover", () => {
+  spanrso5.style.opacity = "1";
+});
+logorso5.addEventListener("mouseout", () => {
+  spanrso5.style.opacity = "0";
 });
