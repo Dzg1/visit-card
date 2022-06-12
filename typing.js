551caf4bc1fcefbc0aa76e1ea2b23b70b47273a7
DzG <100204919+Dzgaza@users.noreply.github.com>
2022-06-12 15:10:02 +0200

    reflex the title in progress

--- typing.js
+++ typing.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing", {
-  strings: ["Css3", "Javascript", "Php  "],
+  strings: [" Html5", "Css3", "Javascript", "Php  "],
   typeSpeed: 100,
   Backspeed: 100,
   loop: true,
